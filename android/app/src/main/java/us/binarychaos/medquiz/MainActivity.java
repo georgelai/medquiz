dab7e7170fd3946a321c6a3c865f304b3a2f4872
Aaron Whitfield <aawhitfield@hotmail.com>
2019-05-08 22:04:42 -0700

    Initial commit

---
+++ android/app/src/main/java/us/binarychaos/medquiz/MainActivity.java
@@ -0,0 +1,13 @@
+package us.binarychaos.medquiz;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
